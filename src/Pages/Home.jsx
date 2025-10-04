29021975dadc271ff4ecb3059507fc57b7cdd2b6
sudipovi <shudipdey73@gmail.com>
2025-10-04 20:28:16 +0600

    5 running

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,23 @@
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+    const [plants, setPlants] = useState([])
+    useEffect(()=>{
+        fetch('https://openapi.programming-hero.com/api/plants')
+            .then(res => res.json())
+            .then(data => setPlants(data?.plants))
+    },[])
+    console.log(plants);
     return (
         <div>
-            <h1>It is From Home</h1>
+            <h1>Plant Card</h1>
+            {
+                plants.map(plant => (
+                    <p key={plant.id}>{plant.name}</p>
+                ))
+            }
         </div>
     );
 };
